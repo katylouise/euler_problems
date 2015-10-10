9d783a59b79fc2b7c1d92bc4f5cb833c5643ca3e
Rebecca Appleyard <rklappleyard@gmail.com>
2015-10-10 17:59:14 +0100

    Refactored primes method

--- Euler Problem3.rb
+++ Euler Problem3.rb
@@ -2,16 +2,19 @@ require 'prime'
 
 def primes_to_n(n)
 	primes = []
-	Prime.each{ |n| primes << n }
+	Prime.each(n / 2){ |n| primes << n }
 	primes
 end
 
 def largest_prime_factor(n)
 	prime_factors = []
-	return prime_factors.max if n == 1
+	if n == 1
+		return prime_factors.max
+	else
+
 	prime_nums = primes_to_n(n)
 	prime_factors = prime_nums.select{ |x| n % x == 0 }
 	prime_factors.max
 end
 
-puts largest_prime_factor(600851475143)
+puts primes_to_n(100)
