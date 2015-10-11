8f8b3947a7190727e3f716c47276f440022f6966
Rebecca Appleyard <rklappleyard@gmail.com>
2015-10-11 19:13:17 +0100

    Need to refactor this more still

--- Euler Problem4.rb
+++ Euler Problem4.rb
@@ -7,19 +7,11 @@ end
 def largest_palindrome
   palindromes = []
   (901..999).each do |n|
-    (901..999).each do |m|
+    (n..999).each do |m|
       palindromes << (n * m) if is_palindrome?(n * m)
     end
   end
-  return palindromes.sort.max
-  # n = 997799
-  # while n > 10201
-  #   if is_palindrome?(n) && !Prime.prime?(n)
-  #     return n
-  #     break
-  #   end
-  #   n -= 1
-  # end
+  palindromes.sort.max
 end
 
 p largest_palindrome
