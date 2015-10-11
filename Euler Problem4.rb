575c6ed0d6a4c1ca0cb5e26cac2accbf5a810741
Rebecca Appleyard <rklappleyard@gmail.com>
2015-10-11 18:09:22 +0100

    Correct solution - now to refactor

--- Euler Problem4.rb
+++ Euler Problem4.rb
@@ -1,7 +1,25 @@
+require 'prime'
+
 def is_palindrome?(n)
   n.to_s.reverse == n.to_s
 end
 
 def largest_palindrome
-
+  palindromes = []
+  (901..999).each do |n|
+    (901..999).each do |m|
+      palindromes << (n * m) if is_palindrome?(n * m)
+    end
+  end
+  return palindromes.sort.max
+  # n = 997799
+  # while n > 10201
+  #   if is_palindrome?(n) && !Prime.prime?(n)
+  #     return n
+  #     break
+  #   end
+  #   n -= 1
+  # end
 end
+
+p largest_palindrome
